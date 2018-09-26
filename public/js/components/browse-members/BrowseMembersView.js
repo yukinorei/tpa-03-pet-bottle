a665d757be690a4eaef9d2ee97792bce1729cd9c
yukinorei <>
2018-09-26 22:23:24 +0900

    constants追加

--- public/js/components/browse-members/BrowseMembersView.js
+++ public/js/components/browse-members/BrowseMembersView.js
@@ -1,4 +1,5 @@
 import { removeChildren } from '../../dom-utils.js';
+import { title, next } from './constants.js';
 
 class BrowseMembersView {
   constructor() {
@@ -32,12 +33,12 @@ class BrowseMembersView {
     const robotThumbEl = document.createElement('IMG');
     robotThumbEl.src = thumbnailUrl;
     const nameTitle = document.createElement('H3');
-    nameTitle.innerHTML = '名前';
+    nameTitle.innerHTML = title.name;
     const NamePhraseEl = document.createElement('P');
     NamePhraseEl.classList.add('member-name');
     NamePhraseEl.innerHTML = name;
     const quoteTitle = document.createElement('H3');
-    quoteTitle.innerHTML = '一言';
+    quoteTitle.innerHTML = title.quote;
     const quotePhraseEl = document.createElement('P');
     quotePhraseEl.classList.add('member-text');
     quotePhraseEl.innerHTML = quote;
@@ -46,7 +47,7 @@ class BrowseMembersView {
     nextButton.addEventListener('click', () => {
       handleBrowseNextMember();
     });
-    nextButton.innerHTML = '次へ';
+    nextButton.innerHTML = next;
     parent.appendChild(child);
     child.appendChild(imageBox);
     imageBox.appendChild(robotThumbEl);
---
+++ public/js/components/browse-members/constants.js
@@ -0,0 +1,6 @@
+export const title = {
+  name: '名前',
+  quote: '一言',
+};
+
+export const next = '次へ';
