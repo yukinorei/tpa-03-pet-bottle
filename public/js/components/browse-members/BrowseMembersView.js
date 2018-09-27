d86d4ea5563f28a96389da4ce5916f37d15a7320
yukinorei <>
2018-09-28 00:43:21 +0900

    修正コミット

--- public/js/components/browse-members/BrowseMembersView.js
+++ public/js/components/browse-members/BrowseMembersView.js
@@ -1,4 +1,11 @@
-import { removeChildren } from '../../dom-utils.js';
+import {
+  removeChildren,
+  createButton,
+  createDiv,
+  createH3,
+  createImg,
+  createP,
+} from '../../dom-utils.js';
 import { title, next } from './constants.js';
 
 class BrowseMembersView {
@@ -26,34 +33,29 @@ class BrowseMembersView {
     this.clearDom();
 
     const parent = document.getElementById('container');
-    const child = document.createElement('DIV');
+    const child = createDiv();
     child.classList.add('member-profile');
-    const imageBox = document.createElement('class');
+    const imageBox = createDiv();
     imageBox.classList.add('member-image-box');
-    const robotThumbEl = document.createElement('IMG');
+    const robotThumbEl = createImg();
     robotThumbEl.src = thumbnailUrl;
-    const nameTitle = document.createElement('H3');
-    nameTitle.innerHTML = title.name;
-    const NamePhraseEl = document.createElement('P');
+    const titleName = createH3(title.name);
+    const NamePhraseEl = createP(name);
     NamePhraseEl.classList.add('member-name');
-    NamePhraseEl.innerHTML = name;
-    const quoteTitle = document.createElement('H3');
-    quoteTitle.innerHTML = title.quote;
-    const quotePhraseEl = document.createElement('P');
+    const titleQuote = createH3(title.quote);
+    const quotePhraseEl = createP(quote);
     quotePhraseEl.classList.add('member-text');
-    quotePhraseEl.innerHTML = quote;
-    const nextButton = document.createElement('button');
+    const nextButton = createButton(next);
     nextButton.classList.add('btn-next-member');
     nextButton.addEventListener('click', () => {
       handleBrowseNextMember();
     });
-    nextButton.innerHTML = next;
     parent.appendChild(child);
     child.appendChild(imageBox);
     imageBox.appendChild(robotThumbEl);
-    child.appendChild(nameTitle);
+    child.appendChild(titleName);
     child.appendChild(NamePhraseEl);
-    child.appendChild(quoteTitle);
+    child.appendChild(titleQuote);
     child.appendChild(quotePhraseEl);
     child.appendChild(nextButton);
   }
